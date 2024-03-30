d9dcbe0c3265f9b65022f8f578634604db533c96
patelkevin18 <patelkevin18102000@gmail.com>
2024-03-30 15:00:31 +0530

    Login Page design completed

--- src/Authentication/Login.js
+++ src/Authentication/Login.js
@@ -5,10 +5,12 @@ import { loginUser } from "../libs/api";
 import { useToasts } from "react-toast-notifications";
 import useToken from "../hooks/useToken";
 import { MyContext } from "../hooks/MyContextProvider";
+import Logo from "../Images/OSI-GRADIENT-LOGO.png";
+
 const Login = () => {
-  const {   setUserDetails } = useToken();
-const { token, updateToken } = useContext(MyContext);
-  
+  const { setUserDetails } = useToken();
+  const { token, updateToken } = useContext(MyContext);
+
   const { addToast } = useToasts();
   const navigate = useNavigate();
   const [inputData, setInputData] = useState({
@@ -25,7 +27,6 @@ const { token, updateToken } = useContext(MyContext);
 
     const userData = await loginUser(inputData);
     if (userData.status) {
-   
       updateToken(userData?.data?.token);
       setUserDetails({
         id: userData?.data?.userDetail?._id,
@@ -56,20 +57,22 @@ const { token, updateToken } = useContext(MyContext);
   }, [navigate, token]);
 
   return (
-    <div className={`login-container`}>
-      <div className="forms-container">
-        <div className="signin-signup">
-          <form
-            onSubmit={(e) => {
-              handleSubmit(e);
-            }}
-            style={{ marginBottom: 20 }}
-            action="#"
-            className="sign-in-form"
+    <div class="container-fluid">
+      <div class="row no-gutter">
+        <div class="col-md-4 " style={{ backgroundColor: "#1D1C21" }}>
+          <div
+            className="main-login-div d-flex justify-content-center align-items-center flex-column"
+            style={{ padding: 30 }}
           >
-            <h2 className="title">Sign in</h2>
-            <div className="input-field">
-              <i className="fas fa-user"></i>
+            <img className="login-logo" src={Logo} alt="logo" />
+
+            <p className="login-text">LOG IN</p>
+            <p className="login-text-sm">
+              Don’t have an account? <Link to={"/signup"}>Sign up</Link>
+            </p>
+
+            <div className="login-form mt-4">
+              <p className="mb-0 pb-1">Email/Username</p>
               <input
                 type="text"
                 name="email"
@@ -79,9 +82,7 @@ const { token, updateToken } = useContext(MyContext);
                 }}
                 placeholder="Email"
               />
-            </div>
-            <div className="input-field">
-              <i className="fas fa-lock"></i>
+              <p className="mb-0 pb-1 mt-3">Password</p>
               <input
                 type="password"
                 name="password"
@@ -92,15 +93,15 @@ const { token, updateToken } = useContext(MyContext);
                 placeholder="Password"
               />
             </div>
-            <input
-              style={{ marginTop: 10, textAlign: "center" }}
-              type="submit"
-              value="Login"
-              className="btn btn-secondary "
-            />
-          </form>
-          <Link to={"/signup"}>Or Sign UP</Link>
+
+            <button className="login-button">LOG IN</button>
+
+            <Link to={"/forgetpassword"} className="mt-2">
+              forgot your password?
+            </Link>
+          </div>
         </div>
+        <div class="col-md-8 d-none d-md-flex bg-image"></div>
       </div>
     </div>
   );
